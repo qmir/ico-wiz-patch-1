452c9f7250c058bbce08eb782336b5b796755f7e
qmir <k-b-s1@ya.ru>
2018-01-28 18:11:21 +0300

    Info about deployed crowdsale with Infura

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../../assets/stylesheets/application.css';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import CrowdsalesList from '../Common/CrowdsalesList'
-import { Loader } from '../Common/Loader'
-import { loadRegistryAddresses } from '../../utils/blockchainHelpers'
-import { ModalContainer } from '../Common/ModalContainer'
+import {Loader} from '../Common/Loader'
+import {loadRegistryAddresses} from '../../utils/blockchainHelpers'
+import {ModalContainer} from '../Common/ModalContainer'
+import {web3Store} from '../stores'
 
 export class Home extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       showModal: false,
@@ -16,22 +17,26 @@ export class Home extends Component {
   }
 
   chooseContract = () => {
-    this.setState({
-      loading: true
-    })
+    const {web3} = web3Store
+    this.setState({loading: true})
+    const addr = '0x3c8DF154241e6917959BcE6Ad1d8E3D3D1B13C64'
+    const net = 'mainnet'
 
-    loadRegistryAddresses()
-      .then(() => {
-        this.setState({
-          loading: false,
-          showModal: true
-        })
+    if (!web3) {
+      loadRegistryAddrsWithInfura(addr,net).then(() => {
+        this.setState({loading: false, showModal: true})
       }, (e) => {
         console.error('There was a problem loading the crowdsale addresses from the registry', e)
-        this.setState({
-          loading: false
-        })
+        this.setState({loading: false})
       })
+    } else {
+      loadRegistryAddresses().then(() => {
+        this.setState({loading: false, showModal: true})
+      }, (e) => {
+        console.error('There was a problem loading the crowdsale addresses from the registry', e)
+        this.setState({loading: false})
+      })
+    }
   }
 
   onClick = crowdsaleAddress => {
@@ -39,77 +44,74 @@ export class Home extends Component {
   }
 
   hideModal = () => {
-    this.setState({ showModal: false })
+    this.setState({showModal: false})
   }
 
-  render () {
-    return (
-      <div>
-        <section className="home">
-          <div className="crowdsale">
-            <div className="container">
-              <h1 className="title">Welcome to ICO Wizard</h1>
-              <p className="description">
+  render() {
+    return (<div>
+      <section className="home">
+        <div className="crowdsale">
+          <div className="container">
+            <h1 className="title">Welcome to ICO Wizard</h1>
+            <p className="description">
               ICO Wizard is a client side tool to create token and crowdsale contracts in five steps. It helps you to publish contracts on the Ethereum network, verify them in Etherscan, create a crowdsale page with stats. For participants, the wizard creates a page to invest into the campaign.
-              <br/>Smart contracts based on <a href="https://github.com/TokenMarketNet/ico">TokenMarket</a> contracts.
+              <br/>Smart contracts based on
+              <a href="https://github.com/TokenMarketNet/ico">TokenMarket</a>
+              contracts.
+            </p>
+            <div className="buttons">
+              <Link to='/1'>
+                <span className="button button_fill">New crowdsale</span>
+              </Link>
+              <div onClick={() => this.chooseContract()} className="button button_outline">Choose Contract</div>
+            </div>
+          </div>
+        </div>
+        <div className="process">
+          <div className="container">
+            <div className="process-item">
+              <div className="step-icons step-icons_crowdsale-contract"></div>
+              <p className="title">Crowdsale Contract</p>
+              <p className="description">
+                Select a strategy for crowdsale contract
               </p>
-              <div className="buttons">
-                <Link to='/1'><span className="button button_fill">New crowdsale</span></Link>
-                <div onClick={() => this.chooseContract()} className="button button_outline">Choose Contract1</div>
-              </div>
+            </div>
+            <div className="process-item">
+              <div className="step-icons step-icons_token-setup"></div>
+              <p className="title">Token Setup</p>
+              <p className="description">
+                Setup token and reserved distribution
+              </p>
+            </div>
+            <div className="process-item">
+              <div className="step-icons step-icons_crowdsale-setup"></div>
+              <p className="title">Crowdsale Setup</p>
+              <p className="description">
+                Setup tiers and crowdsale parameters
+              </p>
+            </div>
+            <div className="process-item">
+              <div className="step-icons step-icons_publish"></div>
+              <p className="title">Publish</p>
+              <p className="description">
+                Get generated code and artifacts for verification in Etherscan
+              </p>
+            </div>
+            <div className="process-item">
+              <div className="step-icons step-icons_crowdsale-page"></div>
+              <p className="title">Crowdsale Page</p>
+              <p className="description">
+                Bookmark this page for the campaign statistics
+              </p>
             </div>
           </div>
-          <div className="process">
-            <div className="container">
-              <div className="process-item">
-                <div className="step-icons step-icons_crowdsale-contract"></div>
-                <p className="title">Crowdsale Contract</p>
-                <p className="description">
-                  Select a strategy for crowdsale contract
-                </p>
-              </div>
-              <div className="process-item">
-                <div className="step-icons step-icons_token-setup"></div>
-                <p className="title">Token Setup</p>
-                <p className="description">
-                  Setup token and reserved distribution
-                </p>
-              </div>
-              <div className="process-item">
-                <div className="step-icons step-icons_crowdsale-setup"></div>
-                <p className="title">Crowdsale Setup</p>
-                <p className="description">
-                  Setup tiers and crowdsale parameters
-                </p>
-              </div>
-              <div className="process-item">
-                <div className="step-icons step-icons_publish"></div>
-                <p className="title">Publish</p>
-                <p className="description">
-                  Get generated code and artifacts for verification in Etherscan
-                </p>
-              </div>
-              <div className="process-item">
-                <div className="step-icons step-icons_crowdsale-page"></div>
-                <p className="title">Crowdsale Page</p>
-                <p className="description">
-                  Bookmark this page for the campaign statistics
-                </p>
-              </div>
-            </div>
-          </div>
-          <ModalContainer
-            title={'Crowdsale List'}
-            description={`The list of your updatable crowdsales. Choose crowdsale address, click Continue and you'll be
-           able to update the parameters of crowdsale.`}
-            hideModal={this.hideModal}
-            showModal={this.state.showModal}
-          >
-            <CrowdsalesList onClick={this.onClick}/>
-          </ModalContainer>
-          <Loader show={this.state.loading}></Loader>
-        </section>
-      </div>
-    );
+        </div>
+        <ModalContainer title={'Crowdsale List'} description={`The list of your updatable crowdsales. Choose crowdsale address, click Continue and you'll be
+           able to update the parameters of crowdsale.`} hideModal={this.hideModal} showModal={this.state.showModal}>
+          <CrowdsalesList onClick={this.onClick}/>
+        </ModalContainer>
+        <Loader show={this.state.loading}></Loader>
+      </section>
+    </div>);
   }
 }
--- src/utils/blockchainHelpers.js
+++ src/utils/blockchainHelpers.js
@@ -1,19 +1,20 @@
-import { incorrectNetworkAlert, noMetaMaskAlert, invalidNetworkIDAlert } from './alerts'
-import { CHAINS, MAX_GAS_PRICE } from './constants'
-import { crowdsaleStore, generalStore, web3Store, contractStore } from '../stores'
-import { fetchFile } from './utils'
+import {incorrectNetworkAlert, noMetaMaskAlert, invalidNetworkIDAlert} from './alerts'
+import {CHAINS, MAX_GAS_PRICE} from './constants'
+import {crowdsaleStore, generalStore, web3Store, contractStore} from '../stores'
+import {fetchFile} from './utils'
 import deploymentStore from '../stores/DeploymentStore'
 
 const DEPLOY_CONTRACT = 1
 const CALL_METHOD = 2
 
-export function checkWeb3 () {
-  const { web3 } = web3Store
+export function checkWeb3() {
+  const {web3} = web3Store
 
   if (!web3) {
-    setTimeout(function () {
+    setTimeout(function() {
       web3Store.getWeb3(web3 => {
-        if (!web3) return noMetaMaskAlert()
+        if (!web3)
+          return noMetaMaskAlert()
         checkMetaMask()
       })
     }, 500)
@@ -24,36 +25,40 @@ export function checkWeb3 () {
 }
 
 const checkMetaMask = () => {
-  const { web3 } = web3Store
+  const {web3} = web3Store
 
-  web3.eth.getAccounts()
-    .then(accounts => {
-      if (accounts.length === 0) return noMetaMaskAlert()
-    })
+  web3.eth.getAccounts().then(accounts => {
+    if (accounts.length === 0)
+      return noMetaMaskAlert()
+  })
 }
 
-export function checkNetWorkByID (_networkIdFromGET) {
+export function checkNetWorkByID(_networkIdFromGET) {
   console.log(_networkIdFromGET)
 
-  if (!_networkIdFromGET) return invalidNetworkIDAlert()
+  if (!_networkIdFromGET)
+    return invalidNetworkIDAlert()
 
-  const { web3 } = web3Store
+  const {web3} = web3Store
   let networkNameFromGET = getNetWorkNameById(_networkIdFromGET)
-  networkNameFromGET = networkNameFromGET ? networkNameFromGET : CHAINS.UNKNOWN
+  networkNameFromGET = networkNameFromGET
+    ? networkNameFromGET
+    : CHAINS.UNKNOWN
 
-  web3.eth.net.getId()
-    .then(_networkIdFromNetwork => {
-      let networkNameFromNetwork = getNetWorkNameById(_networkIdFromNetwork)
-      networkNameFromNetwork = networkNameFromNetwork ? networkNameFromNetwork : CHAINS.UNKNOWN
+  web3.eth.net.getId().then(_networkIdFromNetwork => {
+    let networkNameFromNetwork = getNetWorkNameById(_networkIdFromNetwork)
+    networkNameFromNetwork = networkNameFromNetwork
+      ? networkNameFromNetwork
+      : CHAINS.UNKNOWN
 
-      if (networkNameFromGET !== networkNameFromNetwork) {
-        console.log(networkNameFromGET + '!=' + networkNameFromNetwork)
-        incorrectNetworkAlert(networkNameFromGET, networkNameFromNetwork)
-      }
-    })
+    if (networkNameFromGET !== networkNameFromNetwork) {
+      console.log(networkNameFromGET + '!=' + networkNameFromNetwork)
+      incorrectNetworkAlert(networkNameFromGET, networkNameFromNetwork)
+    }
+  })
 }
 
-export function getNetWorkNameById (_id) {
+export function getNetWorkNameById(_id) {
   switch (parseInt(_id, 10)) {
     case 1:
       return CHAINS.MAINNET
@@ -73,11 +78,13 @@ export function getNetWorkNameById (_id) {
 }
 
 export const calculateGasLimit = (estimatedGas = 0) => {
-  return !estimatedGas || estimatedGas > MAX_GAS_PRICE ? MAX_GAS_PRICE : estimatedGas + 100000
+  return !estimatedGas || estimatedGas > MAX_GAS_PRICE
+    ? MAX_GAS_PRICE
+    : estimatedGas + 100000
 }
 
-export function getNetworkVersion () {
-  const { web3 } = web3Store
+export function getNetworkVersion() {
+  const {web3} = web3Store
 
   if (web3.eth.net && web3.eth.net.getId) {
     return web3.eth.net.getId()
@@ -85,63 +92,56 @@ export function getNetworkVersion () {
   return Promise.resolve(null)
 }
 
-export function setExistingContractParams (abi, addr, setContractProperty) {
-  attachToContract(abi, addr)
-    .then(crowdsaleContract => {
-      crowdsaleContract.token
-        .call(function (err, tokenAddr) {
-          if (err) return console.error(err)
+export function setExistingContractParams(abi, addr, setContractProperty) {
+  attachToContract(abi, addr).then(crowdsaleContract => {
+    crowdsaleContract.token.call(function(err, tokenAddr) {
+      if (err)
+        return console.error(err)
 
-          console.log('tokenAddr:', tokenAddr)
-          setContractProperty('token', 'addr', tokenAddr)
-        })
-
-      crowdsaleContract.multisigWallet
-        .call(function (err, multisigWalletAddr) {
-          if (err) return console.error(err)
-
-          console.log('multisigWalletAddr:', multisigWalletAddr)
-          setContractProperty('multisig', 'addr', multisigWalletAddr)
-        })
+      console.log('tokenAddr:', tokenAddr)
+      setContractProperty('token', 'addr', tokenAddr)
     })
+
+    crowdsaleContract.multisigWallet.call(function(err, multisigWalletAddr) {
+      if (err)
+        return console.error(err)
+
+      console.log('multisigWalletAddr:', multisigWalletAddr)
+      setContractProperty('multisig', 'addr', multisigWalletAddr)
+    })
+  })
 }
 
 export const deployContract = (i, abi, bin, params) => {
-  const { web3 } = web3Store
+  const {web3} = web3Store
   const deployOpts = {
     data: '0x' + bin,
     arguments: params
   }
 
-  return web3.eth.getAccounts()
-    .then(accounts => deployContractInner(accounts, abi, deployOpts))
+  return web3.eth.getAccounts().then(accounts => deployContractInner(accounts, abi, deployOpts))
 }
 
 const deployContractInner = (accounts, abi, deployOpts) => {
   console.log('abi', abi)
 
-  const { web3 } = web3Store
+  const {web3} = web3Store
   const objAbi = JSON.parse(JSON.stringify(abi))
   const contractInstance = new web3.eth.Contract(objAbi)
   const deploy = contractInstance.deploy(deployOpts)
 
-  return deploy.estimateGas({ gas: MAX_GAS_PRICE })
-    .then(
-      estimatedGas => estimatedGas,
-      err => console.log('errrrrrrrrrrrrrrrrr', err)
-    )
-    .then(estimatedGas => {
-      console.log('gas is estimated', estimatedGas)
-      const sendOpts = {
-        from: accounts[0],
-        gasPrice: generalStore.gasPrice,
-        gas: calculateGasLimit(estimatedGas)
-      }
-      return sendTX(deploy.send(sendOpts), DEPLOY_CONTRACT)
-    })
+  return deploy.estimateGas({gas: MAX_GAS_PRICE}).then(estimatedGas => estimatedGas, err => console.log('errrrrrrrrrrrrrrrrr', err)).then(estimatedGas => {
+    console.log('gas is estimated', estimatedGas)
+    const sendOpts = {
+      from: accounts[0],
+      gasPrice: generalStore.gasPrice,
+      gas: calculateGasLimit(estimatedGas)
+    }
+    return sendTX(deploy.send(sendOpts), DEPLOY_CONTRACT)
+  })
 }
 
-export function sendTXToContract (method) {
+export function sendTXToContract(method) {
   return sendTX(method, CALL_METHOD)
 }
 
@@ -150,74 +150,69 @@ let sendTX = (method, type) => {
   let txHash
 
   return new Promise((resolve, reject) => {
-    method
-      .on('error', error => {
-        console.error(error)
-        // https://github.com/poanetwork/ico-wizard/issues/472
-        if (
-          !error.message.includes('Failed to check for transaction receipt')
-          && !error.message.includes('Failed to fetch')
-          && !error.message.includes('Unexpected end of JSON input')
-        ) reject(error)
-      })
-      // This additional polling of tx receipt was made, because users had problems on mainnet: wizard hanged on random
-      // transaction, because there wasn't response from it, no receipt. Especially, if you switch between tabs when
-      // wizard works.
-      // https://github.com/oraclesorg/ico-wizard/pull/364/files/c86c3e8482ef078e0cb46b8bebf57a9187f32181#r152277434
-      .on('transactionHash', _txHash => checkTxMined(_txHash, function pollingReceiptCheck (err, receipt) {
-        if (isMined) return
+    method.on('error', error => {
+      console.error(error)
+      // https://github.com/poanetwork/ico-wizard/issues/472
+      if (!error.message.includes('Failed to check for transaction receipt') && !error.message.includes('Failed to fetch') && !error.message.includes('Unexpected end of JSON input'))
+        reject(error)
+    })
+    // This additional polling of tx receipt was made, because users had problems on mainnet: wizard hanged on random
+    // transaction, because there wasn't response from it, no receipt. Especially, if you switch between tabs when
+    // wizard works.
+    // https://github.com/oraclesorg/ico-wizard/pull/364/files/c86c3e8482ef078e0cb46b8bebf57a9187f32181#r152277434
+      .on('transactionHash', _txHash => checkTxMined(_txHash, function pollingReceiptCheck(err, receipt) {
+      if (isMined)
+        return
         //https://github.com/poanetwork/ico-wizard/issues/480
-        if (
-          err
-          && !err.message.includes('Failed to check for transaction receipt')
-          && !err.message.includes('Failed to fetch')
-          && !err.message.includes('Unexpected end of JSON input')
-        ) return reject(err)
+      if (err && !err.message.includes('Failed to check for transaction receipt') && !err.message.includes('Failed to fetch') && !err.message.includes('Unexpected end of JSON input'))
+        return reject(err)
 
-        txHash = _txHash
-        const typeDisplayName = getTypeOfTxDisplayName(type)
+      txHash = _txHash
+      const typeDisplayName = getTypeOfTxDisplayName(type)
 
-        if (receipt) {
-          if (receipt.blockNumber) {
-            console.log(`${typeDisplayName} ${txHash} is mined from polling of tx receipt`)
-            isMined = true
-            sendTXResponse(receipt, type).then(resolve).catch(reject)
-          } else {
-            repeatPolling()
-          }
+      if (receipt) {
+        if (receipt.blockNumber) {
+          console.log(`${typeDisplayName} ${txHash} is mined from polling of tx receipt`)
+          isMined = true
+          sendTXResponse(receipt, type).then(resolve).catch(reject)
         } else {
           repeatPolling()
         }
+      } else {
+        repeatPolling()
+      }
 
-        function repeatPolling () {
-          console.log(`${typeDisplayName} ${txHash} is still pending. Polling of transaction once more`)
-          setTimeout(() => checkTxMined(txHash, pollingReceiptCheck), 5000)
-        }
-      }))
-      .on('receipt', receipt => {
-        if (isMined) return
+      function repeatPolling() {
+        console.log(`${typeDisplayName} ${txHash} is still pending. Polling of transaction once more`)
+        setTimeout(() => checkTxMined(txHash, pollingReceiptCheck), 5000)
+      }
+    })).on('receipt', receipt => {
+      if (isMined)
+        return
 
-        const typeDisplayName = getTypeOfTxDisplayName(type)
+      const typeDisplayName = getTypeOfTxDisplayName(type)
 
-        console.log(`${typeDisplayName} ${txHash} is mined from Promise`)
-        isMined = true
+      console.log(`${typeDisplayName} ${txHash} is mined from Promise`)
+      isMined = true
 
-        sendTXResponse(receipt, type).then(resolve).catch(reject)
-      })
+      sendTXResponse(receipt, type).then(resolve).catch(reject)
+    })
   })
 
 }
 
 const sendTXResponse = (receipt, type) => {
-  if (0 !== +receipt.status || null === receipt.status) {
-    return type === DEPLOY_CONTRACT ? Promise.resolve(receipt.contractAddress) : Promise.resolve()
+  if (0 !== + receipt.status || null === receipt.status) {
+    return type === DEPLOY_CONTRACT
+      ? Promise.resolve(receipt.contractAddress)
+      : Promise.resolve()
   } else {
-    return Promise.reject({ message: 0 })
+    return Promise.reject({message: 0})
   }
 }
 
 export const checkTxMined = (txHash, _pollingReceiptCheck) => {
-  const { web3 } = web3Store
+  const {web3} = web3Store
 
   web3.eth.getTransactionReceipt(txHash, (err, receipt) => {
     if (receipt)
@@ -240,28 +235,26 @@ const getTypeOfTxDisplayName = (type) => {
   }
 }
 
-export function attachToContract (abi, addr) {
-  const { web3 } = web3Store
+export function attachToContract(abi, addr) {
+  const {web3} = web3Store
 
-  return web3.eth.getAccounts()
-    .then(accounts => {
-      const objAbi = JSON.parse(JSON.stringify(abi))
-      return new web3.eth.Contract(objAbi, addr, { from: accounts[0] })
-    })
+  return web3.eth.getAccounts().then(accounts => {
+    const objAbi = JSON.parse(JSON.stringify(abi))
+    return new web3.eth.Contract(objAbi, addr, {from: accounts[0]})
+  })
 }
 
-function getRegistryAddress () {
-  const { web3 } = web3Store
+function getRegistryAddress() {
+  const {web3} = web3Store
 
-  return web3.eth.net.getId()
-    .then(networkId => {
-      const registryAddressMap = JSON.parse(process.env['REACT_APP_REGISTRY_ADDRESS'] || '{}')
-      return registryAddressMap[networkId]
-    })
+  return web3.eth.net.getId().then(networkId => {
+    const registryAddressMap = JSON.parse(process.env['REACT_APP_REGISTRY_ADDRESS'] || '{}')
+    return registryAddressMap[networkId]
+  })
 }
 
-export function registerCrowdsaleAddress () {
-  const { web3 } = web3Store
+export function registerCrowdsaleAddress() {
+  const {web3} = web3Store
   const toJS = x => JSON.parse(JSON.stringify(x))
 
   const registryAbi = contractStore.registry.abi
@@ -269,55 +262,89 @@ export function registerCrowdsaleAddress () {
 
   const whenRegistryAddress = getRegistryAddress()
 
-  const whenAccount = web3.eth.getAccounts()
-    .then((accounts) => accounts[0])
+  const whenAccount = web3.eth.getAccounts().then((accounts) => accounts[0])
 
-  return Promise.all([whenRegistryAddress, whenAccount])
-    .then(([registryAddress, account]) => {
-      const registry = new web3.eth.Contract(toJS(registryAbi), registryAddress)
-      const opts = { from: account, gasLimit: 100000 }
-
-      return registry.methods
-        .add(crowdsaleAddress)
-        .send(opts)
-    })
-    .then(() => deploymentStore.setAsSuccessful('registerCrowdsaleAddress'))
+  return Promise.all([whenRegistryAddress, whenAccount]).then(([registryAddress, account]) => {
+    const registry = new web3.eth.Contract(toJS(registryAbi), registryAddress)
+    const opts = {
+      from: account,
+      gasLimit: 100000
+    }
+    return registry.methods.add(crowdsaleAddress).send(opts)
+  }).then(() => deploymentStore.setAsSuccessful('registerCrowdsaleAddress'))
 }
 
-function getRegistryAbi () {
+function getRegistryAbi() {
   return fetchFile('./contracts/Registry_flat.abi')
 }
 
-function getRegistryContract () {
+function getRegistryContract() {
   // Get Registry ABI and address
   const whenRegistryAbi = getRegistryAbi().then(JSON.parse)
   const whenRegistryAddress = getRegistryAddress()
 
   // Load Registry contract
-  return Promise.all([whenRegistryAbi, whenRegistryAddress])
-    .then(([abi, address]) => attachToContract(abi, address))
+  return Promise.all([whenRegistryAbi, whenRegistryAddress]).then(([abi, address]) => attachToContract(abi, address))
 }
 
-export function loadRegistryAddresses () {
-  const { web3 } = web3Store
+export function loadRegistryAddresses() {
+  const {web3} = web3Store
   const whenRegistryContract = getRegistryContract()
-  const whenAccount = web3.eth.getAccounts()
-    .then((accounts) => accounts[0])
+  const whenAccount = web3.eth.getAccounts().then((accounts) => accounts[0])
 
-  return Promise.all([whenRegistryContract, whenAccount])
-    .then(([registry, account]) => {
-      return registry.methods.count(account).call()
-        .then((count) => {
-          const crowdsales = []
+  return Promise.all([whenRegistryContract, whenAccount]).then(([registry, account]) => {
+    return registry.methods.count(account).call().then((count) => {
+      const crowdsales = []
 
-          for (let i = 0; i < +count; i++) {
-            crowdsales.push(registry.methods.deployedContracts(account, i).call())
-          }
+      for (let i = 0; i < + count; i++) {
+        crowdsales.push(registry.methods.deployedContracts(account, i).call())
+      }
 
-          return Promise.all(crowdsales)
-        })
+      return Promise.all(crowdsales)
     })
-    .then(crowdsales => {
-      crowdsaleStore.setCrowdsales(crowdsales)
+  }).then(crowdsales => {
+    crowdsaleStore.setCrowdsales(crowdsales)
+  })
+}
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+// Infura
+
+function getRegistryAddressWithInfura(net) {
+  const url = "https://${net}.infura.io/5HfnvZ04q1A0dl6EbwR2";
+  const json = JSON.stringify({"jsonrpc":"2.0","method":"net_version","params":[],"id":67});
+  const options = {
+     method: "POST",
+     headers,
+     body: json
+  }
+  const request = new Request(url, options)
+  const resp = await fetch(request)
+  return resp.json().then(res => {
+    const registryAddressMap = JSON.parse(process.env['REACT_APP_REGISTRY_ADDRESS'] || '{}')
+    return registryAddressMap[res.result]
+  });
+}
+
+function getRegistryContractWithInfura(net) {
+  const whenRegistryAbi = getRegistryAbi().then(JSON.parse)
+  const whenRegistryAddress = getRegistryAddressWithInfura(net)
+  return Promise.all([whenRegistryAbi, whenRegistryAddress]).then(([abi, address]) => attachToContract(abi, address))
+}
+
+export function loadRegistryAddrsWithInfura(acc,net) {
+  const whenRegistryContract = getRegistryContractWithInfura(net)
+  return Promise.all([whenRegistryContract, acc]).then(([registry, account]) => {
+    return registry.methods.count(account).call().then((count) => {
+      const crowdsales = []
+      for (let i = 0; i < + count; i++) {
+        crowdsales.push(registry.methods.deployedContracts(account, i).call())
+      }
+      return Promise.all(crowdsales)
     })
+  }).then(crowdsales => {
+    crowdsaleStore.setCrowdsales(crowdsales)
+  })
 }
