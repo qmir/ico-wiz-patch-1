3a0f90af7ebbb9387a58c829e910e8736fb54699
qmir <k-b-s1@ya.ru>
2018-02-03 18:37:54 +0300

    Check if token is minting

--- src/components/crowdsale/index.js
+++ src/components/crowdsale/index.js
@@ -128,9 +128,8 @@ export class Crowdsale extends React.Component {
     const tokenAddr = contractStore.token.addr
     const tokenAbi = contractStore.token.abi
     const token = new web3.eth.Contract(toJS(tokenAbi), tokenAddr)
-    const mintingFinished = token.methods.mintingFinished((res,err) => {
+    const mintingFinished = token.methods.mintingFinished((res) => {
       console.log(res);
-      console.log(err);
     });
     console.log('token.methods',token.methods);
     // crowsale
